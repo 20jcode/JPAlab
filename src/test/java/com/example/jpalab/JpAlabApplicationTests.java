a98b3ca80b9a3e8a8084f6dad5337aa005f4c6a2
mac <20j.code@gmail.com>
2023-08-13 14:35:36 +0900

    Initial commit

---
+++ src/test/java/com/example/jpalab/JpAlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAlabApplicationTests {
+
+	@Test
+	void contextLoads () {
+	}
+
+}
